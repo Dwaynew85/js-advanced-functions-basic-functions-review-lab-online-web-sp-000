c616e7f83c48743ddb91042165be9a79db91f635
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-05 18:34:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function saturdayFun(activity='roller-skate') {
 let mondayWork = function(activity="go to the office") {
   return `This Monday, I will ${activity}.`
 };
+
+function wrapAdjective(flair="*") {
+  return function(adj='special') {
+    return `You are ${flair}${adj}${flair}!`
+  }
+};
